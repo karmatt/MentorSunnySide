69ae38dcf2d8d015533663b317f703175d212ea2
Karl Matthews <karlmatthews923@gmail.com>
2022-03-09 16:43:35 -0500

    Finished mobile version of the site

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function App() {
+  this.hamburger = document.querySelector(".hamburger");
+  this.nav = document.querySelector(".nav");
+}
+const app = new App();
+app.hamburger.onclick = () => {
+  app.nav.classList.toggle("nav--hidden");
+}
